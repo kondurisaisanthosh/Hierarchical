efa2a746cf0056b4085c189183398189dbc3ed7d
kondurisaisanthosh <konduri.santosh96@gmail.com>
2020-06-06 21:03:13 -0400

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { from } from 'rxjs';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
